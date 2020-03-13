35a0a7c4eed2fab20bda70a7fefe8530e6910b50
Anthony Muli <mulianthony561@gmail.com>
2020-03-13 22:22:58 +0300

    Created a signal to show that a task has been made by the sender

--- awewards/models.py
+++ awewards/models.py
@@ -7,3 +7,17 @@ class Image(models.Model):
 
     def __str__(self):
         return self.description
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='profpics')
+    bio = models.TextField(blank = True)
+    user_id = models.OneToOneField(user, null=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.bio
+
+    def save_user(self):
+        self.save()
+
+    @receiver(post_save,sender=User)
+    def create_user_Profile(sender,instance)
