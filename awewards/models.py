c01d18df7491b6cbde1f7e489958bd0540f84779
Anthony Muli <mulianthony561@gmail.com>
2020-03-13 22:48:51 +0300

    Created a class for projects in the models.py file and added project_name,project_photo,project_description,github_repo,url and the uploader of the project

--- awewards/models.py
+++ awewards/models.py
@@ -20,4 +20,18 @@ class Profile(models.Model):
         self.save()
 
     @receiver(post_save,sender=User)
-    def create_user_Profile(sender,instance)
+    def create_user_Profile(sender,instance,created, **kwargs):
+        if created:
+            Profile.objects.create(user_id=instance)
+
+    @receiver(post_save,sender=User)
+    def save_profile(sender,instance, **kwargs):
+        instance.profile.save()
+
+class Projects(models.Model):
+    project_name = models.CharField(max_length=50, blank=True)
+    project_photo = models.ImageField(uplaod_to='projectpics/')
+    description = models.TextField(max_length=400,blank=True)
+    github_repo = models.CharField(max_length=150, blank=True)
+    url = models.CharField(max_length=60,blank=True)
+    uploader = models.ForeignKey(User, null=True,on_delete=models.CASCADE)
