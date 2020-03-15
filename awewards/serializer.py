1485fc6404a7d0429b207cbb7ea4de4ded2fb12d
Anthony Muli <mulianthony561@gmail.com>
2020-03-15 12:47:20 +0300

    Created serializer.py file to faciitate in conversion of JSON data

---
+++ awewards/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import *
+
+
+class ProjSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = ('project_name', 'project_photo', 'description', 'github_repo', 'url',)
