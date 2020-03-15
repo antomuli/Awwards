5d0fb36cefa73a3f338cc82a2ab8404a1f69ccad
Anthony Muli <mulianthony561@gmail.com>
2020-03-15 10:57:58 +0300

    Replaced the image field with cloudinary field which is an image hosting platform

--- awewards/models.py
+++ awewards/models.py
@@ -1,17 +1,23 @@
+# _*_ coding: utf -8 -*-
+from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Image(models.Model):
-    image = models.ImageField(upload_to='images/')
-    description = models.CharField(max_length=100,blank=true)
+    image = CloudinaryField('images')
+    description = models.CharField(max_length=100,blank=True)
 
     def __str__(self):
         return self.description
 
 class Profile(models.Model):
-    profile_photo = models.ImageField(upload_to='profpics')
+    profile_photo = CloudinaryField('profpics')
     bio = models.TextField(blank = True)
-    user_id = models.OneToOneField(user, null=True,on_delete=models.CASCADE)
+    user_id = models.OneToOneField(User, null=True,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.bio
@@ -20,7 +26,7 @@ class Profile(models.Model):
         self.save()
 
     @receiver(post_save,sender=User)
-    def create_user_Profile(sender,instance,created, **kwargs):
+    def create_user_profile(sender,instance,created, **kwargs):
         if created:
             Profile.objects.create(user_id=instance)
 
@@ -30,8 +36,11 @@ class Profile(models.Model):
 
 class Projects(models.Model):
     project_name = models.CharField(max_length=50, blank=True)
-    project_photo = models.ImageField(uplaod_to='projectpics/')
+    project_photo = CloudinaryField('projectpics/')
     description = models.TextField(max_length=400,blank=True)
     github_repo = models.CharField(max_length=150, blank=True)
     url = models.CharField(max_length=60,blank=True)
     uploader = models.ForeignKey(User, null=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.url
